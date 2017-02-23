208abb5bf5cdf309ac804e562efda277b0363339
cmh <cmh@cmh-Lenovo-C340>
2017-02-23 23:48:18 +0800

    add

---
+++ arithmetic/datastructure/queue/QueueTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package datastructure.queue;
+
+/**
+ * @author cmh
+ * 2017 下午11:47:56
+ */
+public class QueueTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
